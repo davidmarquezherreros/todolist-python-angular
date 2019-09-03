7b964c1252858611df58b3d4e2f381e728e56c93
davidmarquezherreros <davidmarquezherreros@gmail.com>
2019-09-03 18:52:04 +0200

    Added routes configuration

--- frontend/todo-app/src/app/app-routing.module.ts
+++ frontend/todo-app/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'task/:id', component: TaskDetailComponent},
+  { path: 'task', component: TaskDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/todo-app/src/app/app.module.ts
+++ frontend/todo-app/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TaskService } from 'src/services/task-service.service';
+import { TaskListComponent } from './task-list/task-list.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    HeaderComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
